832f6505975a457596eee7e73cf356a897c5a2ef
SmokerMan <Kolyamba831@inbox.ru>
2012-03-15 22:05:30 +0300

    Revert "release 1.7 beta 2"
    This reverts commit 2ad065bd5e92f7c604b11fb0db947ee074881e46.

--- Component/site/views/category/tmpl/default_comment.php
+++ Component/site/views/category/tmpl/default_comment.php
@@ -2,16 +2,18 @@
 /**
  * SMFAQ
  *
- * @package		component for Joomla 2.5+
- * @version		1.7 beta 2
- * @copyright	(C)2009 - 2011 by SmokerMan (http://joomla-code.ru)
+ * @package		component for Joomla 1.6. - 2.5
+ * @version		1.7 beta 1
+ * @copyright	(C)2009 - 2012 by SmokerMan (http://joomla-code.ru)
  * @license		GNU/GPL v.3 see http://www.gnu.org/licenses/gpl.html
  */
 
 // защита от прямого доступа
-defined('_JEXEC') or die('@-_-@'); ?>
+defined('_JEXEC') or die('@-_-@');
+?>
 <div class="title"><?php echo JText::_('COM_SMFAQ_COMMENT_TITLE'); ?></div>
 <textarea name="comment"></textarea><br /><br />
 <input type="hidden" name="token" value="<?php echo JSession::getFormToken(); ?>" />
 <input type="hidden" name="id" value="<?php echo $this->id; ?>" />
 <input type="button" class="button" onclick="SmFaq.Comment(this.form)" value="<?php echo JText::_('COM_SMFAQ_SEND'); ?>" />
+
